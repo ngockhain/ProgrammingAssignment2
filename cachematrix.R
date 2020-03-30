f077a69bf297b91b92717046466785b9ef59d32e
ngockhain <ngockhain@gmail.com>
2020-03-30 15:42:51 +0700

    Done!

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
+library(pracma)
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_mx <- NULL
+    
+    set <- function(mx){x <<- mx}
+    
+    get <- function() x
+    
+    set_inv <- function(inv_mtrx) {inv_mx <<- inv_mtrx}
+    
+    get_inv <- function() inv_mx
+    
+    list(set=set, get=get, set_inv=set_inv, get_inv=get_inv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv_mtrx <- x$get_inv()
+    if(!is.null(inv_mtrx)){
+        message("Loaded inverse matrix from cache.")
+        return(inv_mtrx)
+    }
+    inv_mtrx <- inv(x$get(), ...)
+    x$set_inv(inv_mtrx)
+    inv_mtrx
 }
